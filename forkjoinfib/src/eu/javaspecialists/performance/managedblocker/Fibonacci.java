62d9598cf52235adee58c50a60f0968429acf372
kabutz <heinz@javaspecialists.eu>
2017-05-10 14:20:09 +0200

    demo 2

--- forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
+++ forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
@@ -1,12 +1,15 @@
 package eu.javaspecialists.performance.managedblocker;
 
 import java.math.*;
+import java.util.concurrent.*;
 
 // TODO: Would you like to subscribe to my advanced Java newsletter?
 // TODO: Or are you subscribed already and would like to say "hi"?
 // TODO: Do both here: tinyurl.com/jspring17
 
 // test100_000_000() time = 46953
+// test100_000_000() time = 23844
+
 
 public class Fibonacci {
     public BigInteger f(int n) {
@@ -15,8 +18,14 @@ public class Fibonacci {
 
         int half = (n + 1) / 2;
 
-        BigInteger f0 = f(half-1);
+        RecursiveTask<BigInteger> f0_task = new RecursiveTask<BigInteger>() {
+            protected BigInteger compute() {
+                return f(half-1);
+            }
+        };
+        f0_task.fork();
         BigInteger f1 = f(half);
+        BigInteger f0 = f0_task.join();
 
         if (n % 2 == 1) {
             return f0.multiply(f0).add(f1.multiply(f1));
